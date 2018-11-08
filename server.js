d3c27ee6974b9ca718bee90dfd9fb81a29349374
edscript <edgardo214@gmail.com>
2018-11-07 22:59:58 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+
+app.use((req, res, next) => {
+    var log = `${new Date()} ${req.method} ${req.url}\n`;
+    fs.appendFile('server.log', log, (e) => {if(e)console.log('Error writing the server log file')});
+    console.log(log);
+    next();
+});
+
+
+app.use((req, res, next)=>{
+    res.render('maintenance');
+});
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        bodyText: 'Hello, welcome to my crappie page'
+    });
+});
+
+//about
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({ errorMessage: "Unable to process the request" });
+});
+
+app.listen(3000, () => console.log('Server is up in the port 3000'));
